160e996eb1a71bf128689933d6d396fc885f10f6
Bhupendra <iiitianbhupendra@gmail.com>
2024-01-29 10:38:07 +0530

    Replaced anchor tags with <Link> component from react-router-dom lib. Now the whole page does not get re-rendered when the child components like About, Contact, etc  are called

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { COMPANY_LOGO } from "../utils/constants";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [logIn, setlogIn] = useState(false);
@@ -13,10 +14,10 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-        <li key={0}><a href="/">Home</a></li>
-          <li key={1}><a href="/about" >About Us</a></li>
-          <li key={2}><a href="/contact">Contact Us</a></li>
-          <li key={3}><a href="/my-cart">CartIcon</a></li>
+        <li key={0}><Link to="/">Home</Link></li>
+          <li key={1}><Link to="/about" >About Us</Link></li>
+          <li key={2}><Link to="/contact">Contact Us</Link></li>
+          <li key={3}><Link to="/my-cart">CartIcon</Link></li>
           <li key={4}><button className="log-button" onClick={() => {setlogIn(!logIn)}
           }>
             {logIn? "Logout": "Login"}
