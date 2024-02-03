ef8c672bac9a79874d9667afddbcea1e6ea1c4e3
Bhupendra <iiitianbhupendra@gmail.com>
2024-02-03 15:21:14 +0530

    Learned how to declared multiple state variables in Class Component.

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,16 @@
+import {useState} from "react"
+
 const User = (props) => {
   const {firstname, lastname} = props // Object Destructing
+
+  // Declaring Local state variables in a functional component:
+  const [count, _setcount] = useState(0);
+  const [count2, _setcount2] = useState(2);
+
   return (
     <div className="user-container">
+      <h1 className="user user-name">Count = {count}</h1>
+      <h1 className="user user-name">Count2 = {count2}</h1>
       <h2 className="user user-name">{firstname} {lastname}</h2>
       <h3 className="user user-location">Location: Bhilai</h3>
       <h3 className="user user-social">instagram : @thebhupi</h3>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,15 +2,24 @@ import React from "react";
 
 class UserClass extends React.Component {
   constructor (props) {
-    super(props)
-    // console.log(props)
+    super(props);
+
+    // Declaring Local state variables in a Class component:
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
   }
 
   render() {
     const {first_name, last_name} = this.props // Object Destructing
+
+    const {count, count2} = this.state // Destructuring state object
     
     return (
       <div className="user-container">
+        <h1 className="user user-name">Count = {count}</h1>
+        <h1 className="user user-name">Count2 = {count2}</h1>
         <h2 className="user user-name">{first_name} {last_name}</h2>
         <h3 className="user user-location">Location: Bhilai</h3>
         <h3 className="user user-social">instagram : @thebhupi</h3>
