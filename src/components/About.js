38175632e3f5901f8c163d60ab23f66bbdd6b416
Bhupendra <iiitianbhupendra@gmail.com>
2024-02-03 09:10:51 +0530

    Did some object destructuring: 1

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div className='about-container'>
         <h1>About US</h1>
-        <h2 className="about-title" style={{"font-style": "italic", "color": "orangered"}}>CEO</h2>
+        <h2 className="about-title" style={{"fontStyle": "italic", "color": "orangered"}}>CEO</h2>
         
         <User firstname={"Function"}/>
         <UserClass first_name={"Class"}/>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,12 +4,15 @@ class UserClass extends React.Component {
   constructor (props) {
     super(props)
     this.firstName = this.props.first_name;
+    console.log(props)
   }
 
   render() {
+    const firstname = this.firstName
+    
     return (
       <div className="user-container">
-        <h2 className="user user-name">{this.firstName} Chouhan</h2>
+        <h2 className="user user-name">{firstname} Chouhan</h2>
         <h3 className="user user-location">Location: Bhilai</h3>
         <h3 className="user user-social">instagram : @thebhupi</h3>
         <p className="user-bio-para">Comming from UserClass.js</p>
